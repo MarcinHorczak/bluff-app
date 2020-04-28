cb8badb960c8387ae3b03b16a12707cb35a0ee80
Marcin Horczak <marcin.horczak@euvic.pl>
2020-04-28 17:42:36 +0200

    feat: add auth and users services and modules

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+    userId: number,
+    username: string,
+    password: string
+};
+
+@Injectable()
+export class UsersService {
+  private users: User[];
+
+  constructor() {
+    this.users = [
+      {
+        userId: 1,
+        username: 'admin',
+        password: 'password'
+      }
+    ]
+  }
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username)
+  }
+}
