228a70b0883c2a53d02be971df340bb2485ab7a3
Marcin Horczak <marcin.horczak@euvic.pl>
2020-04-28 18:50:34 +0200

    feat: jwt authorization

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
-
+import { UsersService, User } from '../users/users.service'
+import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt'
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && bcrypt.compare(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null
+  }
+
+  async register(userData: User): Promise<any> {
+    return await this.usersService.create(userData)
+  }
+
+  async login(user: User): Promise<any> {
+    const payload = { username: user.username, sub: user.userId }
+
+    return {
+      accessToken: this.jwtService.sign(payload)
+    }
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt'
 
 export type User = {
     userId: number,
@@ -22,5 +23,19 @@ export class UsersService {
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find(user => user.username === username)
+  }
+
+  async create(userData: User): Promise<any> {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
+    const userId = Date.now();
+
+    this.users.push({
+      userId: userId,
+      username: userData.username,
+      password: hashedPassword
+    });
+
+    return true
   }
 }
